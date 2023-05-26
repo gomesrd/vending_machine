d542244a8a655756fdf480907e762c1d920faaa6
Douglas Santos <douglaspg@outlook.com>
2023-05-25 22:09:23 -0300

    alteracao_codigo

--- vending_machine.cpp
+++ vending_machine.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int calcule_valor_transacao();
 int verifica_disponibilidade_produto();
+int lista_invetario();
+int lista_faturamento();
+int verifica_administrador();
 
 int main()
 {
@@ -18,20 +21,48 @@ int main()
         cout << "3 - Chocolate ---------- R$ 3,00" << endl;
         cout << "4 - Bolacha ---------- R$ 3,25" << endl;
         cout << "5 - Salgadinho ---------- R$ 4,50" << endl;
+        cout << "99 - ADMINISTRADOR" << endl;
         cout << "9 - SAIR" << endl;
         cin >> operacao;
 
-        cout << "Informe a quantia em dinheiro inserida na máquina:" << endl;
-        cin >> quantia_inserida;
+        if (operacao = 99)
+        {
+            if ()
+            {
+                cout << "1 - LISTAR INVETÁRIO" << endl;
+                cout << "2 - REPOSIÇÃO DE PRODUTOS" << endl;
+                cout << "3 - FATURAMENTO" << endl;
+            }
+        }
 
-        calculo_transacao()
+        else
+        {
+            verifica_disponibilidade_produto();
 
+            cout << "Informe a quantia em dinheiro inserida na máquina:" << endl;
+
+            cin >> quantia_inserida;
+
+            calcule_valor_transacao();
+
+            cout << "O seu troco é de R$ " << endl;
+        }
     } while (operacao != 9);
 
     system("read");
     return 0;
 }
 
+// FUNÇÃO RESPONSÁVEL POR VERIFICAR SE O PRODUTO SELECIONADO ESTÁ DISPONÍVEL NO ESTOQUE
+int verifica_administrador();
+
+// FUNÇÃO RESPONSÁVEL POR VERIFICAR SE O PRODUTO SELECIONADO ESTÁ DISPONÍVEL NO ESTOQUE
+int verifica_disponibilidade_produto(int produto)
+{
+    // buscar
+}
+
+// FUNÇÃO RESPONSÁVEL POR RECEBER O VALOR INSERIDO NA MÁQUINA, VERIFICAR O VALOR DO PRODUTO COMPRADO E CALCULAR O TROCO
 int calcule_valor_transacao(int valor_produto, int quantia_inserida, int troco)
 {
     troco = quantia_inserida - valor_produto;
@@ -39,6 +70,17 @@ int calcule_valor_transacao(int valor_produto, int quantia_inserida, int troco)
     return troco;
 }
 
-int verifica_disponibilidade_produto(int produto)
+// FUNÇÃO RESPONSÁVEL POR REPOR OS PRODUTOS QUE ESTÃO EM FALTA NO ESTOQUE
+int reposicao_produto()
+{
+}
+
+// FUNÇÃO RESPONSÁVEL POR LISTAR O INVETÁRIO DE PRODUTOS
+int lista_invetario()
+{
+}
+
+// FUNÇÃO RESPONSÁVEL POR LISTAR O FATURAMENTO DA MÁQUINA DE VENDAS
+int lista_faturamento()
 {
 }
