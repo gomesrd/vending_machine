6ac3cd939033683e0d71af0e0bbf880075642272
Douglas Santos <douglaspg@outlook.com>
2023-05-30 15:17:05 -0300

    Ajuste no codigo

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # vending_machine
- Projeto de uma máquina de vendas automática idealizado em C++ para a matéria de Algorítimo e Lógica de Programação da FATEC-RP
+
+Projeto de uma máquina de vendas automática idealizado em C++ para a matéria de Algorítimo e Lógica de Programação da FATEC-RP
 
 # Os alunos responsáveis pela elaboração do projeto foram:
 
@@ -10,10 +11,35 @@
     RA: 2840482313013
 
 # Branch
+
     Main
     Development
     Murilo
-    Douglas 
-    
+    Douglas
+
 # Requisitos do programa
-    
+
+1. Quando o programa inicializar será exibido ao usuário uma lista de produtos, contendo a descrição e preço, além das opções do modo administrador e sair do programa, além de uma mensagem para que o usuário escola o produto.
+
+2. Quando o usuário escolher o produto, será chamado uma função para verificar se há estoque do produto selecionado.
+
+3. Caso não haja, será solicitado ao usuário que escolha outro produto.
+
+4. Caso haja, será solicitado ao usuario para que insira o dinheiro na máquina
+
+5. Após, será chamada uma função para calcular o valor do troco, subtraindo o valor inserido do valor do produto selecionado.
+
+6. Caso o valor inserido seja insulficiente, será solicitado ao usuário que insira o valor correto. Caso novamente seja inserido o valor incorreto, será encerrada a venda.
+
+7. Com o pagamento, será deduzido da quantidade total a unidade comprada pelo usuário.
+
+8. Após, será informado o troco do usuário, bem como a mensagem para que ele retire o produto.
+
+9. Programa inicia novamente.
+
+MODO ADMINISTRADOR
+
+1. Quando entrar no modo administrado, será exibida a mensagem para: a. repor produtos faltantes; b. listar um invetário dos produtos e suas quantidade; c. listar quanto a máquina já faturou e ainda pode faturar.
+
+
+A. Quando quiser repor produtos, o administrador deve informar o nome do produto e a quantidade que deseja repor.
--- vending_machine.cpp
+++ vending_machine.cpp
@@ -2,123 +2,111 @@
 
 using namespace std;
 
-void inicia_os_precos_e_a_quantidade_de_cada_produto();
-int calcule_valor_transacao();
-int verifica_disponibilidade_produto();
-int lista_invetario();
-int lista_faturamento();
-int verifica_administrador();
+void modo_vendas()
+{
+}
+
+void modo_administrador()
+{
+    cout << "Bem vindo ao modo administrador!" << endl;
+    cout << "1 - Repor o estoque" << endl;
+    cout << "2 - Realizar Inventário" << endl;
+    cout << "3 - Faturamento da máquina" << endl;
+}
 
 int main()
 {
-    int operacao = 0, quantia_inserida = 0;
-    int senha, opcoes; //Variaveis adm
-    char simnao;
 
-    int quanti_agua;
-    float *p_agua;
-    float preco_agua;
-
-    p_agua = &preco_agua;
-
+    bool verifica_estoque = false;
+    int operacao, troco = 0, qtdade_agua = 2;
+    float preco_agua = 3, dinheiro_inserido = 0;
 
     do
     {
-        cout << "-------Bem vindo a máquina de vendas automáticas FATEC-RP-----------" << endl;
-        cout << "Digite o número do produto que deseja comprar:" << endl;
-        cout << "1 - Água ---------- R$ 3,50" << endl;
+        cout << "-------Bem vindo a máquina de vendas automáticas FATEC-RP-----------\n";
+        cout << "1 - Água ------------------ R$ 3,00" << endl;
         cout << "2 - Refrigerante ---------- R$ 4,00" << endl;
-        cout << "3 - Chocolate ---------- R$ 3,00" << endl;
-        cout << "4 - Bolacha ---------- R$ 3,25" << endl;
-        cout << "5 - Salgadinho ---------- R$ 4,50" << endl;
-        cout << "99 - ADMINISTRADOR" << endl;
-     // cout << "9 - SAIR" << endl;
+        cout << "3 - Chocolate ------------- R$ 3,00" << endl;
+        cout << "4 - Bolacha --------------- R$ 3,25" << endl;
+        cout << "5 - Salgadinho ------------ R$ 4,50" << endl;
+        cout << "8 - ADMINISTRADOR" << endl;
+        cout << "9 - SAIR" << endl;
+        cout << "Digite o número do produto que deseja comprar: ";
         cin >> operacao;
 
-        if (operacao = 99)
-        {  
-            cout << "Senha: ";  //Senha para acesssar o modo administrador: 1234
-            cin >> senha;
-            
-                if (senha == 1234)
+        switch (operacao)
+        {
+
+        case 1:
+            verifica_estoque = qtdade_agua >= 1;
+            if (verifica_estoque == true)
+            {
+                cout << "Insira o dinheiro na máquina: ";
+                cin >> dinheiro_inserido;
+            }
+            else
+            {
+                cout << "Não há estoque do produto selecionado, selecione outro produto.\n\n\n";
+                break;
+            }
+            if (dinheiro_inserido < preco_agua)
+            {
+                do
                 {
-                    cout << "1 - LISTAR INVETÁRIO" << endl;
-                    cout << "2 - REPOSIÇÃO DE PRODUTOS" << endl;
-                    cout << "3 - FATURAMENTO" << endl;
-                    cin >> opcoes;
+                    cout << "O valor inserido é insulficiente para pagamento, por favorinsira o valor correto:";
+                    cin >> dinheiro_inserido;
 
-                        switch (opcoes)
-                        {
-                            case 1:                              
+                } while (dinheiro_inserido < preco_agua);
 
-                            case 2:
+                troco = dinheiro_inserido - preco_agua;
+                qtdade_agua = qtdade_agua - 1;
+                cout << "Ainda restam " << qtdade_agua << endl;
 
-                            case 3:
-                        }
+                if (troco == 0)
+                {
+                    cout << "****Retire o seu produto!****\n\n\n";
                 }
                 else
                 {
-                    //Voltar para o inicio do programa
+
+                    cout << "****O seu troco é de R$ " << troco << "****" << endl;
+                    cout << "****Retire o seu produto!****\n\n\n";
                 }
+            }
+
+            else
+            {
+                troco = dinheiro_inserido - preco_agua;
+                qtdade_agua = qtdade_agua - 1;
+                cout << "Ainda restam " << qtdade_agua << endl;
+
+                if (troco == 0)
+                {
+                    cout << "****Retire o seu produto!****\n\n\n";
+                }
+                else
+                {
+
+                    cout << "****O seu troco é de R$ " << troco << "****" << endl;
+                    cout << "****Retire o seu produto!****\n\n\n";
+                }
+            }
+            break;
+
+        case 8:
+            modo_administrador();
+            break;
+
+        case 9:
+            cout << "Obrigado pela visita, até mais!" << endl;
+            break;
+
+        default:
+            cout << "Opção inválida, selecione novamente!\n\n\n"
+                 << endl;
         }
 
-        else
-        {
-            verifica_disponibilidade_produto();
-
-            cout << "Informe a quantia em dinheiro inserida na máquina:" << endl;
-
-            cin >> quantia_inserida;
-
-            calcule_valor_transacao();
-
-            cout << "O seu troco é de R$ " << endl;
-        }
-            cout << "Deseja compra mais algum produto? (s/n): ";
-            cin >> simnao;
-
-    } while (simnao == 's' || simnao 'S');  	//se a resposta for sim ele volta para a pagina principal
-
-    cout << "Obrigado por comprar, volte sempre" << endl;
-
+    } while (operacao != 9);
     system("read");
     return 0;
-}
-
-// FUNÇÃO PARA INICIAR OS PREÇOS E AS QUANTIDADES DOS PRODUTOS NO INICIO DO PROGRAMA
-void inicia_os_precos_e_a_quantidade_de_cada_produto()
-{
-    *preco_agua = 3.50;
-}
-
-// FUNÇÃO RESPONSÁVEL POR VERIFICAR SE O PRODUTO SELECIONADO ESTÁ DISPONÍVEL NO ESTOQUE
-int verifica_administrador();
-
-// FUNÇÃO RESPONSÁVEL POR VERIFICAR SE O PRODUTO SELECIONADO ESTÁ DISPONÍVEL NO ESTOQUE
-int verifica_disponibilidade_produto(int produto)
-{
-    // buscar
-}
-
-// FUNÇÃO RESPONSÁVEL POR RECEBER O VALOR INSERIDO NA MÁQUINA, VERIFICAR O VALOR DO PRODUTO COMPRADO E CALCULAR O TROCO
-int calcule_valor_transacao(int valor_produto, int quantia_inserida, int troco)
-{
-    troco = quantia_inserida - valor_produto;
-
-    return troco;
-}
-
-// FUNÇÃO RESPONSÁVEL POR REPOR OS PRODUTOS QUE ESTÃO EM FALTA NO ESTOQUE
-int reposicao_produto()
-{
-}
-
-// FUNÇÃO RESPONSÁVEL POR LISTAR O INVETÁRIO DE PRODUTOS
-int lista_invetario()
-{
-}
-
-// FUNÇÃO RESPONSÁVEL POR LISTAR O FATURAMENTO DA MÁQUINA DE VENDAS
-int lista_faturamento()
-{
 }
