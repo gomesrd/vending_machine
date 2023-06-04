4240c2b35b8f37f25d21b20882fb0165c8958ac9
MuriloVianna <murivianna@gmail.com>
2023-06-04 10:57:32 -0300

    Comentarios adicionados

--- vending_machine.cpp
+++ vending_machine.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-#define produtos_totais 5
+#define produtos_totais 5       //Define quantos produtos a máquina pode ter 
 
 bool estoque_produto = false;
 int senha, operacao_compra = 0, operacao_adm = 0, troco = 0;
 float total_vendas = 0, dinheiro_inserido = 0, vendas_futuras = 0;
 
-struct Produto
+struct Produto      //Struct usada para listar os produtos
 {
     int codigo;
     string descricao;
@@ -16,11 +16,11 @@ struct Produto
     float quantidade;
 };
 
-Produto lista[5];
+Produto lista[5];       //Vetor que armazena os produtos
 
-void troca_pagina();
+void troca_pagina();        //Função que limpa o código na tela
 
-int mostra_produtos()
+int mostra_produtos()       //Função que lista os produtos disponíveis na máquina
 {
 
     cout << "\n\n\n----------------------------- Bem vindo a máquina de vendas automáticas FATEC-RP -----------------------------\n\n\n";
@@ -33,13 +33,13 @@ int mostra_produtos()
     cout << "9 - DESLIGAR" << endl;
     cout << "\n\nDigite o código do produto que deseja comprar: ";
     cin >> operacao_compra;
-    operacao_compra -= 1;
+    operacao_compra -= 1;       //Operação feita porque o contador começa em 0
 
-    troca_pagina();
+    troca_pagina();     //Função que limpa o código na tela
     return operacao_compra;
 }
 
-int reposicao_estoque();
+int reposicao_estoque();        //Iniciando as funções antes da main
 void inventario();
 void faturamento();
 void modo_administrador();
@@ -52,7 +52,7 @@ int operacao_vendas();
 
 int main()
 {
-    lista[0].codigo = 1;
+    lista[0].codigo = 1;                    //Armazenando os produtos no vetor com a struct
     lista[0].descricao = "Agua";
     lista[0].preco = 3;
     lista[0].quantidade = 0;
@@ -79,9 +79,9 @@ int main()
 
     do
     {
-        mostra_produtos();
+        mostra_produtos();          //Função que lista os produtos disponíveis na máquina
 
-        switch (operacao_compra)
+        switch (operacao_compra)        //Switch que define qual opção foi selecionada na máquina
         {
 
         case 0:
@@ -125,7 +125,7 @@ int main()
     return 0;
 }
 
-int reposicao_estoque()
+int reposicao_estoque()     //Função para repor os itens da máquina
 {
     int produto_selecionado = 0;
     float nova_qtdade = 0;
@@ -133,7 +133,7 @@ int reposicao_estoque()
     while (produto_selecionado != 99)
     {
         cout << "\n\n\n----------------------------- REPOSIÇÃO DE ESTOQUE ----------------------------- \n\n";
-        for (int i = 0; i < produtos_totais; i++)
+        for (int i = 0; i < produtos_totais; i++)           //Lista os produtos disponíveis na máquina
         {
             cout << lista[i].codigo << " - " << lista[i].descricao << endl;
         }
@@ -143,11 +143,11 @@ int reposicao_estoque()
         cout << "\n\nDigite o código do produto que deseja repor: ";
         cin >> produto_selecionado;
 
-        if (produto_selecionado == 9)
+        if (produto_selecionado == 9)       //Se "9" for selecionado ele volta para o menu principal
         {
             break;
         }
-        else if (produto_selecionado > produtos_totais)
+        else if (produto_selecionado > produtos_totais)     //Se o operador digitar um número que não existe na lista
         {
             cout << "O código informado não existe, tente novamente.";
             continue;
@@ -158,7 +158,7 @@ int reposicao_estoque()
         cout << "Digite a nova quantidade: ";
         cin >> nova_qtdade;
 
-        lista[produto_selecionado].quantidade += nova_qtdade;
+        lista[produto_selecionado].quantidade += nova_qtdade;       //Soma a quantidade adicionada a quantidade anterior  
 
         cout << "\n\nOPERAÇÃO EFETUADA COM SUCESSO!\n\n";
         cout << "Foram adicionadas " << nova_qtdade << " unidades no estoque do produto " << lista[produto_selecionado].descricao << endl;
@@ -166,12 +166,12 @@ int reposicao_estoque()
         cout << "\nPressione 'ENTER' para realizar nova reposição" << endl;
 
         system("read || pause");
-        troca_pagina();
+        troca_pagina();         
     };
     return 0;
 }
 
-void inventario()
+void inventario()       //Lista a quantidade de cada produto disponível na máquina
 {
     cout << "\n\n\n----------------------------- INVENTÁRIO ----------------------------- \n\n";
     cout << "O estoque atual possui os seguintes produtos e quantidades: " << endl;
@@ -182,30 +182,29 @@ void inventario()
     cout << "\nPressione 'ENTER' para retornar ao painel do administrador" << endl;
 
     system("read || pause");
-    troca_pagina();
+    troca_pagina();     
 }
 
-void faturamento()
+void faturamento()      //Calcula o quanto a máquina ja faturou e ainda pode faturar
 
 {
     for (int i = 0; i < produtos_totais; i++)
     {
-        vendas_futuras += lista[i].preco * lista[i].quantidade;
+        vendas_futuras += lista[i].preco * lista[i].quantidade;     //Calcula o quanto máquina ainda pode faturar
     }
 
     cout << "\n\n\n----------------------------- FATURAMENTO -----------------------------\n\n";
-    // vendas_futuras = qtdade_agua * preco_agua;
     cout << "Total faturado com as vendas já realizadas: R$ " << total_vendas << endl;
     cout << "Total a ser faturado com vendas futuras: R$ " << vendas_futuras << endl;
     cout << "\nPressione 'ENTER' para retornar ao painel do administrador" << endl;
 
     system("read || pause");
-    troca_pagina();
+    troca_pagina();     
 }
 
 void modo_administrador()
 {
-    cout << "Você está no Modo Administrador. \n\nPor favor, digite a senha: ";
+    cout << "Você está no Modo Administrador. \n\nPor favor, digite a senha: ";     //Para entrar no modo administrador é necessário inserir a senha (1234)
     cin >> senha;
 
     if (senha != 1234)
@@ -215,7 +214,7 @@ void modo_administrador()
     else
     {
 
-        troca_pagina();
+        troca_pagina();     //Função que limpa o código na tela
         do
         {
             cout << "\n\n\n----------------------------- Bem vindo ao Modo Administrador! -----------------------------\n\n";
@@ -256,17 +255,17 @@ void modo_administrador()
     }
 }
 
-void compra_efetuada_com_troco()
+void compra_efetuada_com_troco()        //Função se tiver troco na compra do produto
 {
     cout << "\n****  Compra realizada com suceso! **** " << endl;
     cout << "\n**** O seu troco é de R$ " << troco << " **** " << endl;
     cout << "\n**** Retire o seu produto! **** " << endl;
     cout << "\n **** Pressione 'ENTER' para realizar um nova compra! **** " << endl;
     system("read || pause");
-    troca_pagina();
+    troca_pagina();     
 }
 
-void compra_efetuada_sem_troco()
+void compra_efetuada_sem_troco()        //Função se não tiver troco na compra do produto
 {
     cout << "\n****  Compra realizada com suceso! **** " << endl;
     cout << "\n**** Retire o seu produto! **** " << endl;
@@ -275,35 +274,35 @@ void compra_efetuada_sem_troco()
     troca_pagina();
 }
 
-bool verifica_estoque(int operacao_compra)
+bool verifica_estoque(int operacao_compra)      //Função que verifica se tem disponibilidade do produto no estoque
 {
     estoque_produto = lista[operacao_compra].quantidade >= 1;
     return estoque_produto;
 }
 
-float calculo_troco(float dinheiro_inserido, int operacao_compra)
+float calculo_troco(float dinheiro_inserido, int operacao_compra)       //Função que calcula o troco
 {
     troco = dinheiro_inserido - lista[operacao_compra].preco;
     return troco;
 }
 
-void abatimento_estoque(int operacao_compra)
+void abatimento_estoque(int operacao_compra)        //Função que diminiu uma quantidade do produto comprado
 {
     lista[operacao_compra].quantidade -= 1;
 }
 
-float calculo_total_vendas(int operacao_compra)
+float calculo_total_vendas(int operacao_compra)     //Função que calcula o quanto a máquina já vendeu
 {
     total_vendas += lista[operacao_compra].preco;
     return total_vendas;
 }
 
-int operacao_vendas()
+int operacao_vendas()       //Função que efetua todas a funções durante a compra
 {
 
     verifica_estoque(operacao_compra);
 
-    if (estoque_produto == false)
+    if (estoque_produto == false)       //Se não tiver estoque do produto selecionado na máquina
     {
         cout << "\nNão há estoque do produto selecionado, selecione outro produto.";
     }
@@ -341,7 +340,7 @@ int operacao_vendas()
     return 0;
 }
 
-void troca_pagina()
+void troca_pagina()     //Função que limpa o código na tela
 {
-    system("cls || clear");
+    system("cls || clear");     //Limpa o código na tela, para dar a impressão de entrar em outra página
 }
